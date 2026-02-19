5aa12e1f04926ced51b4db8fb87e9b985c2c65e3
Cristhian Andres Salinas <113159638+CristhianSalinas@users.noreply.github.com>
2026-02-19 04:20:26 +0000

    Ajuste visual

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function crearCard(url) {
   card.classList.add("card");
 
   card.innerHTML = `
-    <img src="${data.sprites.front_default}" alt="${data.name}">
+    <img src="${data.sprites.front_default}" alt="${data.name}" />
     <p>${data.name.toUpperCase()}</p>
   `;
 
@@ -34,14 +34,13 @@ async function crearCard(url) {
 function mostrarDetalle(pokemon) {
   detalle.innerHTML = `
     <h2>${pokemon.name.toUpperCase()}</h2>
-    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
     <p><strong>Altura:</strong> ${pokemon.height}</p>
     <p><strong>Peso:</strong> ${pokemon.weight}</p>
     <p><strong>HP:</strong> ${pokemon.stats[0].base_stat}</p>
     <p><strong>Ataque:</strong> ${pokemon.stats[1].base_stat}</p>
     <p><strong>Defensa:</strong> ${pokemon.stats[2].base_stat}</p>
   `;
-
   detalle.scrollIntoView({ behavior: "smooth" });
 }
 
