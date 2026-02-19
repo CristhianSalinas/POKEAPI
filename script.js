00410b7b5189134019ae5720d9d67cffad289e49
Cristhian Andres Salinas <113159638+CristhianSalinas@users.noreply.github.com>
2026-02-19 03:58:57 +0000

    Android Cambios

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const lista = document.getElementById("lista");
 const detalle = document.getElementById("detalle");
 
-// Cargar primeros 12 Pokémon
 async function cargarLista() {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=15");
     const data = await res.json();
 
     data.results.forEach(pokemon => {
@@ -19,8 +18,8 @@ async function crearCard(url) {
     card.classList.add("card");
 
     card.innerHTML = `
-        <h4>${data.name.toUpperCase()}</h4>
         <img src="${data.sprites.front_default}">
+        <p>${data.name}</p>
     `;
 
     card.addEventListener("click", () => mostrarDetalle(data));
@@ -34,13 +33,12 @@ function mostrarDetalle(pokemon) {
         <img src="${pokemon.sprites.front_default}">
         <p><strong>Altura:</strong> ${pokemon.height}</p>
         <p><strong>Peso:</strong> ${pokemon.weight}</p>
-        <p><strong>Tipos:</strong> 
-            ${pokemon.types.map(t => t.type.name).join(", ")}
-        </p>
         <p><strong>HP:</strong> ${pokemon.stats[0].base_stat}</p>
         <p><strong>Ataque:</strong> ${pokemon.stats[1].base_stat}</p>
         <p><strong>Defensa:</strong> ${pokemon.stats[2].base_stat}</p>
     `;
+
+    detalle.scrollIntoView({ behavior: "smooth" });
 }
 
 cargarLista();
